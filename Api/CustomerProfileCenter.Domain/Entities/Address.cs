ffe60cbccb3ab671df6dfcbcf604e1baba5aae72
Cristiano Cunha <contato@cristianoprogramador.com>
2023-03-15 18:49:32 -0300

    Cria a base de endereço

--- Api/CustomerProfileCenter.Domain/Entities/Address.cs
+++ Api/CustomerProfileCenter.Domain/Entities/Address.cs
@@ -1,6 +1,17 @@
+using CustomerProfileCenter.Domain.ValueObjects;
+
 namespace CustomerProfileCenter.Domain.Entities;
 
 public class Address
 {
-    
+    public Address(Cep cep, string number, string? complement)
+    {
+        Cep = cep;
+        Number = number;
+        Complement = complement;
+    }
+
+    public Cep Cep { get; }
+    public string Number { get; }
+    public string? Complement { get; }
 }
