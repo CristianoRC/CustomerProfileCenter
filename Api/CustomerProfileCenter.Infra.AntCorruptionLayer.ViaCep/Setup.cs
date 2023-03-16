b88173b8e8de007631fc8a9d45407e23cfe8f644
Cristiano Cunha <contato@cristianoprogramador.com>
2023-03-16 09:16:26 -0300

    Configura Client do refit - Retry e Circuit Breaker

--- Api/CustomerProfileCenter.Infra.AntCorruptionLayer.ViaCep/Setup.cs
+++ Api/CustomerProfileCenter.Infra.AntCorruptionLayer.ViaCep/Setup.cs
@@ -2,7 +2,9 @@ using CustomerProfileCenter.Infra.AntCorruptionLayer.ViaCep.ViaCep;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
+using Polly.CircuitBreaker;
 using Polly.Contrib.WaitAndRetry;
+using Polly.Extensions.Http;
 using Polly.Retry;
 using Refit;
 
@@ -13,18 +15,24 @@ public static class Setup
     public static IServiceCollection AddViaCep(this IServiceCollection services, IConfiguration config)
     {
         services.AddRefitClient<IViaCepClient>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(config["ViaCep"]));
-        //.AddPolicyHandler();
-        //Adiciona Retry e Circuit Breaker
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri(config["ViaCep"]))
+            .AddPolicyHandler(GetRetryPolicy())
+            .AddPolicyHandler(GetCircuitBreakerPolicy());
         return services;
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
     }
 
     private static AsyncRetryPolicy<HttpResponseMessage> GetRetryPolicy()
     {
         var delay = Backoff.DecorrelatedJitterBackoffV2(medianFirstRetryDelay: TimeSpan.FromSeconds(1), retryCount: 5);
-        /*return HttpPolicyExtensions
+        return HttpPolicyExtensions
             .HandleTransientHttpError()
-            .WaitAndRetryAsync(delay);*/
-        throw new NotImplementedException();
+            .WaitAndRetryAsync(delay);
     }
 }
