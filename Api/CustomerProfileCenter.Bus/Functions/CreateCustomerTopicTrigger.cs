5f238462e8465d90ca58f50d35717319377a6da1
Cristiano Cunha <contato@cristianoprogramador.com>
2023-03-15 21:36:08 -0300

    Separa diretório apenas para Functions

--- Api/CustomerProfileCenter.Bus/Functions/CreateCustomerTopicTrigger.cs
+++ Api/CustomerProfileCenter.Bus/Functions/CreateCustomerTopicTrigger.cs
@@ -1,15 +1,14 @@
-using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace CustomerProfileCenter.Bus;
+namespace CustomerProfileCenter.Bus.Functions;
 
 public static class CreateCustomerTopicTrigger
 {
     [Function("CreateCustomerTopicTrigger")]
-    public static void Run([RabbitMQTrigger("queue")] string message, ILogger log, FunctionContext context)
+    public static void Run([RabbitMQTrigger("example", ConnectionStringSetting = "RabbitMQConnectionString")] string message,
+        ILogger log, FunctionContext context)
     {
-        //TODO: Colocar connection string na chave rabbitMQConnection
         var logger = context.GetLogger("CreateCustomerTopicTrigger");
         logger.LogInformation($"C# Queue trigger function processed: {message}");
     }
