227a9f01afedffbb90ff0161990ce856009fb71e
Cristiano Cunha <contato@cristianoprogramador.com>
2023-03-15 23:22:34 -0300

    Configura DI na Function

--- Api/CustomerProfileCenter.Bus/Program.cs
+++ Api/CustomerProfileCenter.Bus/Program.cs
@@ -1,6 +1,16 @@
+using CustomerProfileCenter.Domain;
+using CustomerProfileCenter.Infra.AntCorruptionLayer.ViaCep;
+using CustomerProfileCenter.Infra.Data;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
+    .ConfigureServices((builderContext, services) =>
+    {
+        services
+            .AddViaCep(builderContext.Configuration)
+            .AddData(builderContext.Configuration)
+            .AddDomain();
+    })
     .ConfigureFunctionsWorkerDefaults()
     .Build();
 
