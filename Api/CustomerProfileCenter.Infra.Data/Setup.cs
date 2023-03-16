fc2a01be558bb32988cfd9a1630ca1ba8273577c
Cristiano Cunha <contato@cristianoprogramador.com>
2023-03-16 08:20:19 -0300

    Configura DI do Redis

--- Api/CustomerProfileCenter.Infra.Data/Setup.cs
+++ Api/CustomerProfileCenter.Infra.Data/Setup.cs
@@ -1,12 +1,22 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace CustomerProfileCenter.Infra.Data;
 
 public static class Setup
 {
-    public static IServiceCollection AddData(this IServiceCollection service, IConfiguration config)
+    public static IServiceCollection AddData(this IServiceCollection services, IConfiguration config)
     {
-        return service;
+        ConfigureRedis(services, config);
+
+
+        return services;
+    }
+
+    private static void ConfigureRedis(IServiceCollection services, IConfiguration config)
+    {
+        var multiplexer = ConnectionMultiplexer.Connect(config["RedisConnectionString"]);
+        services.AddSingleton<IConnectionMultiplexer>(multiplexer);
     }
 }
