d67781db7256ccbbc739d604db45ae0a7cab3ca1
Cristiano Cunha <contato@cristianoprogramador.com>
2023-03-17 23:56:00 -0300

    Cria bash script para criação das queues

--- Configuration/config-rabbitmq.sh
+++ Configuration/config-rabbitmq.sh
@@ -1,2 +1,5 @@
-echo "olá Mundo"
-echo "$RabbitMQConnectionString"
+echo "Iniciando configuração do RabbitMQ"
+./rabbitmqadmin -u $user -p $password -H $host declare exchange name=CreateCustomer type=fanout
+./rabbitmqadmin -u $user -p $password -H $host declare queue name=CreateCustomerTopicTrigger
+./rabbitmqadmin -u $user -p $password -H $host declare binding source=CreateCustomer destination=CreateCustomerTopicTrigger
+echo "Configuração do RabbitMQ Finalizada"
