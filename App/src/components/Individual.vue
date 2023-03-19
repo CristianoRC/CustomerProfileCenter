f6550333ffdcfb848a2932f9a2da06bd53f6781f
Cristiano Cunha <contato@cristianoprogramador.com>
2023-03-19 10:10:54 -0300

    Implementa tela de Cadastro de Individual

--- App/src/components/Individual.vue
+++ App/src/components/Individual.vue
@@ -11,22 +11,36 @@
                     @focusout="getCep()"></v-text-field>
                 <v-text-field v-model="fields.address.number" label="Numero"></v-text-field>
                 <v-text-field v-model="fields.address.complement" label="Complemento"></v-text-field>
-                <v-text-field v-model="cep.city" disabled label="Cidade"></v-text-field>
                 <v-text-field v-model="cep.state" disabled label="Estado"></v-text-field>
+                <v-text-field v-model="cep.city" disabled label="Cidade"></v-text-field>
                 <v-text-field v-model="cep.neighborhood" disabled label="Bairro"></v-text-field>
-
-
             </form>
         </v-container>
         <v-card-actions>
             <v-container>
                 <v-row justify="end">
-                    <v-btn color="primary" elevation="3" :disabled="loading">Cadastrar</v-btn>
+                    <v-btn @click="createCustomer" color="primary" elevation="3" :disabled="loading">Cadastrar</v-btn>
                 </v-row>
             </v-container>
         </v-card-actions>
     </v-card>
-    <pre>{{ fields }}</pre>
+    <v-snackbar color="orange-darken-2" v-model="showError" timeout="2000">
+        {{ error }}
+        <template v-slot:actions>
+            <v-btn variant="text" @click="showError = false">
+                Fechar
+            </v-btn>
+        </template>
+    </v-snackbar>
+
+    <v-snackbar color="blue-darken-2" v-model="showSuccess" timeout="2000">
+        Cliente Cadastrado com Sucesso
+        <template v-slot:actions>
+            <v-btn variant="text" @click="showSuccess = false">
+                Fechar
+            </v-btn>
+        </template>
+    </v-snackbar>
 </template>
 
 <script lang='ts'>
@@ -40,19 +54,47 @@ export default defineComponent({
             this.$data.loading = true;
             const cep = this.$data.fields.address.cep;
             const url = `http://customerprofile-api.cristianoprogramador.com/Address/${cep}`;
-            
+
             axios.get(url).then(response => {
-                console.log(response);
-                this.$data.cep.city = ""
-                this.$data.cep.neighborhood = ""
-                this.$data.cep.state = ""
-            }).finally(() => { this.$data.loading = false; })
+                this.$data.cep.city = response.data.content.city
+                this.$data.cep.neighborhood = response.data.content.neighborhood
+                this.$data.cep.state = response.data.content.uf
+            }).catch(error => {
+                this.$data.showError = true;
+                if (error.response.status === 400)
+                    this.$data.error = error.response.data.error.errorMessage;
+                else
+                    this.$data.error = "Ocorreu um erro inesperado ao obter inforamções do CEP.";
+            }).finally(() => {
+                this.$data.loading = false;
+            })
+        },
+        createCustomer: function () {
+            this.$data.loading = true;
+            const url = `http://customerprofile-api.cristianoprogramador.com/Customer`;
+
+            axios.post(url, this.$.data.fields).then(response => {
+                this.$data.showSuccess = true;
+
+            }).catch(error => {
+                this.$data.showError = true;
+                if (error.response.status === 409)
+                    this.$data.error = "Cliente já cadastrado.";
+                else
+                    this.$data.error = error.response.data.errorMessage;
+
+            }).finally(() => {
+                this.$data.loading = false;
+            })
         }
     },
     components: {
     },
     data: () => ({
         loading: false,
+        showError: false,
+        showSuccess: false,
+        error: "error",
         fields: {
             name: "",
             document: {
@@ -64,9 +106,10 @@ export default defineComponent({
                 number: "",
                 complement: ""
             },
-            birthday: "",
+            birthday: null,
             phonenumber: "",
-            emailAddress: ""
+            emailAddress: "",
+            corporateName: "",
         },
         cep: {
             city: "",
